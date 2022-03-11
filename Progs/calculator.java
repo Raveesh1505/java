7feefcc3b70d5639865d2cbd1b9cc8292961c2f2
Raveesh Yadav <raveeshyadav8@outlook.com>
2022-03-11 10:56:50 +0530

    Day 1

---
+++ Progs/calculator.java
@@ -0,0 +1,15 @@
+public class calculator{
+    public static void main(String[] args){
+        int num1 = 7, num2 = 5, sum, multiply, divide, subt;
+
+        sum = num1 + num2;
+        multiply = num1*num2;
+        divide = num1/num2;
+        subt = num1-num2;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Product = " + multiply);
+        System.out.println("Divide = " + divide);
+        System.out.println("Difference = " + subt);
+    }
+}
